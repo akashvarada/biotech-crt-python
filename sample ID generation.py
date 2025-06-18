a9d95882d872762cf4bf8b291ca3533404a4342c
akashvarada <akash.varada07@gmail.com>
2025-06-18 14:09:48 +0530

    Delete sample ID generation.py

--- sample ID generation.py
+++
@@ -1,9 +0,0 @@
-sample_prefix=int(input("Enter the Lab sample prefix : "))
-sample_format=""
-num=int(input("Enter the no.of ID's you have to generate: "))
-list=[]
-for i in range(0,num):
-    sample_format=(f"LAB2025-0{sample_prefix+1}")
-    list.append(sample_format)
-    sample_prefix+=1
-print(list)
